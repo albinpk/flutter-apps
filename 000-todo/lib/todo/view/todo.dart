ea136af0cb8d2621a44da506f5ad97636ada4fbc
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-07-24 19:47:08 +0530

    feat(todo): create AppRouter class
    create static method `onGenerate` in AppRouter
    create static name field in TodoPage

--- 000-todo/lib/todo/view/todo.dart
+++ 000-todo/lib/todo/view/todo.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
+  /// ```
+  /// name = '/';
+  /// ```
+  static const name = '/';
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
