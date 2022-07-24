b400e00e441f97e1f27772275d291d4255112cdc
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-07-24 20:22:27 +0530

    feat(todo): create TodoView with CheckboxListTile

--- 000-todo/lib/todo/view/todo.dart
+++ 000-todo/lib/todo/view/todo.dart
@@ -10,8 +10,47 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Todo Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo'),
+      ),
+      body: const TodoView(),
+    );
+  }
+}
+
+class TodoView extends StatelessWidget {
+  const TodoView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        CheckboxListTile(
+          value: true,
+          title: Text(
+            'Todo 1',
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium
+                ?.copyWith(decoration: TextDecoration.lineThrough),
+          ),
+          controlAffinity: ListTileControlAffinity.leading,
+          onChanged: (_) {},
+        ),
+        CheckboxListTile(
+          value: false,
+          title: Text(
+            'Todo 2',
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium
+                ?.copyWith(fontWeight: FontWeight.w500),
+          ),
+          controlAffinity: ListTileControlAffinity.leading,
+          onChanged: (_) {},
+        ),
+      ],
     );
   }
 }
