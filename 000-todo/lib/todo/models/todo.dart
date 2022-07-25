1969379be7a0ec403281dbce3839778023d902b2
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-07-25 08:49:57 +0530

    refactor(todo): Todo extends Equatable

--- 000-todo/lib/todo/models/todo.dart
+++ 000-todo/lib/todo/models/todo.dart
@@ -1,6 +1,7 @@
+import 'package:equatable/equatable.dart';
 import 'package:uuid/uuid.dart';
 
-class Todo {
+class Todo extends Equatable {
   final String id;
   final String title;
   final bool isDone;
@@ -10,4 +11,7 @@ class Todo {
     required this.title,
     this.isDone = false,
   }) : id = id ?? const Uuid().v4();
+
+  @override
+  List<Object> get props => [id, title, isDone];
 }
