80afb76d1d1e54498c6bd9f4f9408a68641885cd
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-07-25 08:14:38 +0530

    feat(todo): add id field to Tood model
    test: update Todo model test

--- 000-todo/test/todo/models/todo_test.dart
+++ 000-todo/test/todo/models/todo_test.dart
@@ -4,18 +4,18 @@ import 'package:todo/todo/models/models.dart';
 void main() {
   group('Todo model', () {
     test('should have given title', () {
-      const todo = Todo(title: 'todo1');
+      final todo = Todo(title: 'todo1');
       expect(todo.title, 'todo1');
     });
 
     test('default value of [isDone] in false', () {
-      const todo = Todo(title: 'title');
+      final todo = Todo(title: 'title');
       expect(todo.isDone, isFalse);
     });
 
     test('change [isDone] to given value', () {
-      const todo1 = Todo(title: 'title', isDone: true);
-      const todo2 = Todo(title: 'title', isDone: false);
+      final todo1 = Todo(title: 'title', isDone: true);
+      final todo2 = Todo(title: 'title', isDone: false);
 
       expect(todo1.isDone, isTrue);
       expect(todo2.isDone, isFalse);
