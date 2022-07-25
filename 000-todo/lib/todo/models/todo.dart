486fe1f6e9ad78ea267cf8d406e3de699120fabc
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-07-25 08:14:38 +0530

    feat(todo): add id field to Tood model
    test: update Todo model test

--- 000-todo/lib/todo/models/todo.dart
+++ 000-todo/lib/todo/models/todo.dart
@@ -1,9 +1,12 @@
+import 'package:uuid/uuid.dart';
+
 class Todo {
+  final String id;
   final String title;
   final bool isDone;
 
-  const Todo({
+  Todo({
     required this.title,
     this.isDone = false,
-  });
+  }) : id = const Uuid().v4();
 }
