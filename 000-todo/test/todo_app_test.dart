2b4f4da16050f42ccc9be3e51a0f997c827b5249
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-04 15:16:01 +0530

    test(todo): add test for title in TodoApp

--- 000-todo/test/todo_app_test.dart
+++ 000-todo/test/todo_app_test.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todo/pages/pages.dart';
 import 'package:todo/todo_app.dart';
 
 void main() {
-  testWidgets('app should have MateriaApp widget', (tester) async {
-    await tester.pumpWidget(const TodoApp());
+  testWidgets(
+    'TodoApp should have MateriaApp with title and TodoPage widget',
+    (tester) async {
+      await tester.pumpWidget(const TodoApp());
 
-    expect(find.byType(MaterialApp), findsOneWidget);
-  });
+      final materialApp = find.byType(MaterialApp);
+
+      expect(materialApp, findsOneWidget);
+      expect(find.byType(TodoPage), findsOneWidget);
+      expect(tester.widget<MaterialApp>(materialApp).title, 'Todo');
+    },
+  );
 }
