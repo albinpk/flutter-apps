638004a6aee8ab31c653ae21a6ddf6054ab1ce15
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-06 23:28:17 +0530

    feat(todo): create feature todo status
    shows todo status (`finished/total`) in TodoPageAppBar

--- 000-todo/lib/widgets/todo_page_app_bar.dart
+++ 000-todo/lib/widgets/todo_page_app_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/todo_cubit.dart';
 
 class TodoPageAppBar extends StatelessWidget implements PreferredSizeWidget {
   const TodoPageAppBar({super.key});
@@ -7,6 +10,29 @@ class TodoPageAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: const Text('Todo'),
+      actions: [
+        Center(
+          widthFactor: 2,
+          child: BlocBuilder<TodoCubit, TodoState>(
+            buildWhen: (previous, current) {
+              return previous.todos.length != current.todos.length ||
+                  previous.todos.where((t) => t.isDone).length !=
+                      current.todos.where((t) => t.isDone).length;
+            },
+            builder: (context, state) {
+              final isDoneCount = state.todos.where((t) => t.isDone).length;
+              final status = '$isDoneCount/${state.todos.length}';
+              return Text(
+                status,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium
+                    ?.copyWith(color: Colors.white),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 
