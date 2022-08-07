cff46741243d7e8536e7431669969ce253b073a4
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-07 16:25:20 +0530

    feat(todo): create `description` field in Todo model

--- 000-todo/lib/models/todo_model.dart
+++ 000-todo/lib/models/todo_model.dart
@@ -6,22 +6,25 @@ import 'package:uuid/uuid.dart';
 class Todo extends Equatable {
   final String id;
   final String title;
+  final String description;
   final bool isDone;
 
   Todo({
     String? id,
     required this.title,
+    this.description = '',
     this.isDone = false,
   })  : assert(title.trim().isNotEmpty, 'Todo [title] must not be empty'),
         id = id ?? const Uuid().v4();
 
   @override
-  List<Object> get props => [id, title, isDone];
+  List<Object?> get props => [id, title, description, isDone];
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'id': id,
       'title': title,
+      'description': description,
       'isDone': isDone,
     };
   }
@@ -30,6 +33,7 @@ class Todo extends Equatable {
     return Todo(
       id: map['id'] as String,
       title: map['title'] as String,
+      description: map['description'] as String,
       isDone: map['isDone'] as bool,
     );
   }
@@ -42,11 +46,13 @@ class Todo extends Equatable {
 
   Todo copyWith({
     String? title,
+    String? description,
     bool? isDone,
   }) {
     return Todo(
       id: id,
       title: title ?? this.title,
+      description: description ?? this.description,
       isDone: isDone ?? this.isDone,
     );
   }
