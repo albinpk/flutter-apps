06dcdeab767e7a3fe3f1bb45a3ea3e52b3f84f60
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-11 19:48:29 +0530

    feat(todo): get todos from localstorage
    create getTodo() method in TodoCubit and emit
    TodoFetched state (with todos from localstorage).
    
    initialize localstorage instance from main() function
    and pass to TodoApp
    
    create TodoCubit with LocalStorageTodoRepository
    and immediately call getTodo() method

--- 000-todo/lib/todo_app.dart
+++ 000-todo/lib/todo_app.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:localstorage/localstorage.dart';
 
 import 'cubit/todo_cubit.dart';
 import 'pages/pages.dart';
+import 'repositories/repositories.dart';
 
 class TodoApp extends StatelessWidget {
-  const TodoApp({super.key});
+  const TodoApp({
+    super.key,
+    required this.localStorage,
+  });
+
+  final LocalStorage localStorage;
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider<TodoCubit>(
-      create: (context) => TodoCubit(),
+      create: (context) => TodoCubit(
+        repository: LocalStorageTodoRepository(localStorage),
+      )..getTodos(),
       child: const MaterialApp(
         title: 'Todo',
         home: TodoPage(),
