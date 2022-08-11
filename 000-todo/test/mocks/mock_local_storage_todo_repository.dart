22bb7b4180f826a30e1a38c8d564733d00df31a8
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-11 20:26:35 +0530

    test(todo): create `MockLocalStorageTodoRepository`

---
+++ 000-todo/test/mocks/mock_local_storage_todo_repository.dart
@@ -0,0 +1,5 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:todo/repositories/repositories.dart';
+
+class MockLocalStorageTodoRepository extends Mock
+    implements LocalStorageTodoRepository {}
