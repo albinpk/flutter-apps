cce36033488010be5c444a39f487c69a347168bc
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-12 17:28:12 +0530

    test(todo): fix TodoApp test by passing mock storage

--- 000-todo/test/todo_app_test.dart
+++ 000-todo/test/todo_app_test.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:todo/pages/pages.dart';
 import 'package:todo/todo_app.dart';
 
+class _MockLocalStorage extends Mock implements LocalStorage {}
+
 void main() {
+  late LocalStorage localStorage;
+
+  setUp(() {
+    localStorage = _MockLocalStorage();
+  });
+
   testWidgets(
     'TodoApp should have MateriaApp with title and TodoPage widget',
     (tester) async {
-      await tester.pumpWidget(const TodoApp());
+      await tester.pumpWidget(TodoApp(localStorage: localStorage));
 
       final materialApp = find.byType(MaterialApp);
 
