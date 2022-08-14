41dffe55310d5f4801ef4de87376e47a64e6b217
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-14 11:44:40 +0530

    feat(todo): responsive todo status

--- 000-todo/lib/widgets/todo_page_app_bar.dart
+++ 000-todo/lib/widgets/todo_page_app_bar.dart
@@ -9,32 +9,52 @@ class TodoPageAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text('Todo'),
-      actions: [
-        Center(
-          widthFactor: 2,
-          child: BlocBuilder<TodoCubit, TodoState>(
-            buildWhen: (previous, current) {
-              return previous.todos.length != current.todos.length ||
-                  previous.doneCount != current.doneCount;
-            },
-            builder: (context, state) {
-              if (state.todos.isEmpty) return const SizedBox.shrink();
-
-              return Text(
-                '${state.doneCount}/${state.todos.length}',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleMedium
-                    ?.copyWith(color: Colors.white),
-              );
-            },
-          ),
-        ),
-      ],
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const Text('Todo'),
+          if (MediaQuery.of(context).size.width > 600) ...const [
+            SizedBox(width: 20),
+            _TodoStatus(),
+          ]
+        ],
+      ),
+      actions: MediaQuery.of(context).size.width > 600
+          ? null
+          : const [
+              Center(
+                widthFactor: 2,
+                child: _TodoStatus(),
+              ),
+            ],
     );
   }
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _TodoStatus extends StatelessWidget {
+  const _TodoStatus({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TodoCubit, TodoState>(
+      buildWhen: (previous, current) {
+        return previous.todos.length != current.todos.length ||
+            previous.doneCount != current.doneCount;
+      },
+      builder: (context, state) {
+        if (state.todos.isEmpty) return const SizedBox.shrink();
+
+        return Text(
+          '${state.doneCount}/${state.todos.length}',
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium
+              ?.copyWith(color: Colors.white),
+        );
+      },
+    );
+  }
 }
