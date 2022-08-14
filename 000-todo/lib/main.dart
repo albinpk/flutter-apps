f3947a5bd621dd5f73a8096c23ccd2b557478838
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-14 20:06:30 +0530

    refactor(todo): wrap TodoApp with TestHelper

--- 000-todo/lib/main.dart
+++ 000-todo/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:localstorage/localstorage.dart';
 
 import 'todo_app.dart';
+import 'utils/test_helper.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,5 +12,10 @@ Future<void> main() async {
   final isLocalStorageReady = await localStorage.ready;
   assert(isLocalStorageReady);
 
-  runApp(TodoApp(localStorage: localStorage));
+  runApp(
+    RepositoryProvider<TestHelper>(
+      create: (context) => const TestHelper(),
+      child: TodoApp(localStorage: localStorage),
+    ),
+  );
 }
