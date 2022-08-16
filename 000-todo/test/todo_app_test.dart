54c99847c15f2565a0c4b63ab0de364db1c4c168
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-16 17:57:18 +0530

    test(todo): refactor todo_app_test.dart

--- 000-todo/test/todo_app_test.dart
+++ 000-todo/test/todo_app_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:mocktail/mocktail.dart';
@@ -8,25 +7,23 @@ import 'package:todo/todo_app.dart';
 class _MockLocalStorage extends Mock implements LocalStorage {}
 
 void main() {
-  late LocalStorage localStorage;
+  group('TodoApp', () {
+    late LocalStorage localStorage;
 
-  setUp(() {
-    localStorage = _MockLocalStorage();
+    setUp(() {
+      localStorage = _MockLocalStorage();
+    });
+
+    testWidgets(
+      'should have a TodoPage and should call localstorage.getItems',
+      (tester) async {
+        await tester.pumpWidget(
+          TodoApp(localStorage: localStorage),
+        );
+        expect(find.byType(TodoPage), findsOneWidget);
+        // localStorage.getItem() will called from getTodos() method in TodoCubit
+        verify(() => localStorage.getItem(any())).called(1);
+      },
+    );
   });
-
-  testWidgets(
-    'TodoApp should have MateriaApp with title and TodoPage widget '
-    'and should call todoCubit.getTodos() when BlocProvider created',
-    (tester) async {
-      await tester.pumpWidget(TodoApp(localStorage: localStorage));
-
-      final materialApp = find.byType(MaterialApp);
-
-      expect(materialApp, findsOneWidget);
-      expect(find.byType(TodoPage), findsOneWidget);
-      expect(tester.widget<MaterialApp>(materialApp).title, 'Todo');
-      // localStorage.getItem() will called from getTodos() method in TodoCubit
-      verify(() => localStorage.getItem(any())).called(1);
-    },
-  );
 }
