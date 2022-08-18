6e7931f3613db15131e5e95252e2da3c24ba1c34
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-08-18 18:03:07 +0530

    refactor(todo): set minimum window size in desktop

--- 000-todo/lib/main.dart
+++ 000-todo/lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:localstorage/localstorage.dart';
+import 'package:window_size/window_size.dart';
 
 import 'todo_app.dart';
+import 'utils/extensions/target_platform_extension.dart';
 import 'utils/test_helper.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (defaultTargetPlatform.isDesktop) {
+    setWindowTitle('Todo');
+    setWindowMinSize(const Size(400, 450));
+  }
 
   final localStorage = LocalStorage('000-todo.json');
   final isLocalStorageReady = await localStorage.ready;
