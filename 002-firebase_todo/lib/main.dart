18a08bd9b9e34dab484d873692e44784d266120a
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-20 18:04:08 +0530

    refactor(firebase-todo): using firestore emulator

--- 002-firebase_todo/lib/main.dart
+++ 002-firebase_todo/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_todo/firebase_todo.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -10,6 +12,10 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (kDebugMode) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
 
   runApp(const FirebaseTodo());
 }
