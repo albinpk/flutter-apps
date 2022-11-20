c2f1be75e977fd8b82757d762c972640d3a2072a
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-20 19:54:36 +0530

    refactor(firebase-todo): add JSON serialization to `Todo`

--- 002-firebase_todo/lib/features/todos/models/todo_model.dart
+++ 002-firebase_todo/lib/features/todos/models/todo_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// The todo model.
 class Todo {
   Todo({
@@ -5,4 +7,21 @@ class Todo {
   });
 
   final String title;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+    };
+  }
+
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(
+      title: map['title'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Todo.fromJson(String source) =>
+      Todo.fromMap(json.decode(source) as Map<String, dynamic>);
 }
