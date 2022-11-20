c474541ee51702227b0a90140d746ceffbc452b1
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-20 17:08:52 +0530

    feat(firebase-todo): create `HomeView`

---
+++ 002-firebase_todo/lib/firebase_todo.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_todo/features/home/presentation/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseTodo extends StatelessWidget {
+  const FirebaseTodo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
