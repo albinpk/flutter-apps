3ac9f23766dc8acf859421bffd30b6db8d1cdd5e
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-20 19:58:44 +0530

    feat(firebase-todo): create `TodoForm`
    show todos in TodosView (using StreamBuilder).

---
+++ 002-firebase_todo/lib/features/todos/presentation/views/todo_form.dart
@@ -0,0 +1,73 @@
+import 'package:firebase_todo/features/todos/models/todo_model.dart';
+import 'package:firebase_todo/features/todos/presentation/views/todos_view.dart';
+import 'package:flutter/material.dart';
+
+class TodoForm extends StatefulWidget {
+  const TodoForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<TodoForm> createState() => _TodoFormState();
+}
+
+class _TodoFormState extends State<TodoForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _title = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Add new todo',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              autofocus: true,
+              decoration: const InputDecoration(
+                label: Text('Title'),
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value?.trim().isEmpty ?? true) return 'Title is required!';
+                return null;
+              },
+              onSaved: (newValue) => _title = newValue!.trim(),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('CANCEL'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _onSave,
+                  child: const Text('SAVE'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onSave() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      todosRef.add(Todo(title: _title));
+      Navigator.pop(context);
+    }
+  }
+}
--- 002-firebase_todo/lib/features/todos/presentation/views/todos_view.dart
+++ 002-firebase_todo/lib/features/todos/presentation/views/todos_view.dart
@@ -1,13 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_todo/features/todos/models/todo_model.dart';
+import 'package:firebase_todo/features/todos/presentation/views/todo_form.dart';
 import 'package:flutter/material.dart';
+
+final todosRef = FirebaseFirestore.instance
+    .collection('todos')
+    .withConverter<Todo>(
+      fromFirestore: (snapshot, options) => Todo.fromMap(snapshot.data() ?? {}),
+      toFirestore: (value, options) => value.toMap(),
+    );
 
 class TodosView extends StatelessWidget {
   const TodosView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Todos'),
+    return Scaffold(
+      body: StreamBuilder(
+        stream: todosRef.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          final docs = snapshot.data!.docs;
+
+          if (docs.isEmpty) {
+            return const Center(child: Text('No todos found!'));
+          }
+
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              return Text(docs[index].data().title);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return const Dialog(
+                child: TodoForm(),
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
