c867c37a5a6b2b97865adb4bc9df13cbf4873373
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 14:39:19 +0530

    refactor(jsonplaceholder): create `Post` model

---
+++ 003-jsonplaceholder-example/lib/features/posts/models/post_model.dart
@@ -0,0 +1,14 @@
+/// Post model.
+class Post {
+  Post({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+}
