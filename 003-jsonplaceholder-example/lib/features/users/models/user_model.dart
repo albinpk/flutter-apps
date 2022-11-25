448a6addf76ca296fcb3c170f2b45a29720748df
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 11:24:26 +0530

    refactor(jsonplaceholder): create `User` model

---
+++ 003-jsonplaceholder-example/lib/features/users/models/user_model.dart
@@ -0,0 +1,18 @@
+/// User model.
+class User {
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.phone,
+    required this.website,
+  });
+
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final String phone;
+  final String website;
+}
