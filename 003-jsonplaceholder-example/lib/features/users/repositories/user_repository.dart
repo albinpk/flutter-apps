92936ec65bd5852a85431c938d6af34a2fb82828
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 12:30:31 +0530

    feat(jsonplaceholder): implement `getAllUsers` method

---
+++ 003-jsonplaceholder-example/lib/features/users/repositories/user_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+
+import '../models/user_model.dart';
+
+class UserRepository {
+  /// Base url.
+  static const _base = 'https://jsonplaceholder.typicode.com';
+
+  /// Fetch users from api.
+  Future<List<User>> getAllUsers() async {
+    try {
+      final res = await http.get(Uri.parse('$_base/users'));
+      final rawUsers = json.decode(res.body) as List;
+      return rawUsers.map((e) => User.fromMap(e)).toList();
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<User> getUserById(int id) async {
+    throw UnimplementedError();
+  }
+}
