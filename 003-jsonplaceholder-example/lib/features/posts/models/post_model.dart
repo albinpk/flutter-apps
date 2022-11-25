cb3c57413e13df20f1649aeda49be0aad1e028a7
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 14:59:09 +0530

    refactor(jsonplaceholder): generate json serialization for `Post` model

--- 003-jsonplaceholder-example/lib/features/posts/models/post_model.dart
+++ 003-jsonplaceholder-example/lib/features/posts/models/post_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// Post model.
 class Post {
   Post({
@@ -11,4 +13,27 @@ class Post {
   final int userId;
   final String title;
   final String body;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'userId': userId,
+      'title': title,
+      'body': body,
+    };
+  }
+
+  factory Post.fromMap(Map<String, dynamic> map) {
+    return Post(
+      id: map['id'] as int,
+      userId: map['userId'] as int,
+      title: map['title'] as String,
+      body: map['body'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Post.fromJson(String source) =>
+      Post.fromMap(json.decode(source) as Map<String, dynamic>);
 }
