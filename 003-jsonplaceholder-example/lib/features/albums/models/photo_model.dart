d2a609d268fe4e68d3e6d4d9c215b2d17642ae04
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 20:53:57 +0530

    refactor(jsonplaceholder): create `Photo` model

---
+++ 003-jsonplaceholder-example/lib/features/albums/models/photo_model.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+class Photo {
+  Photo({
+    required this.id,
+    required this.albumId,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  final int id;
+  final int albumId;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'albumId': albumId,
+      'title': title,
+      'url': url,
+      'thumbnailUrl': thumbnailUrl,
+    };
+  }
+
+  factory Photo.fromMap(Map<String, dynamic> map) {
+    return Photo(
+      id: map['id'] as int,
+      albumId: map['albumId'] as int,
+      title: map['title'] as String,
+      url: map['url'] as String,
+      thumbnailUrl: map['thumbnailUrl'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Photo.fromJson(String source) =>
+      Photo.fromMap(json.decode(source) as Map<String, dynamic>);
+}
