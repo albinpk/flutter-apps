b0f143264bcefa7d7b3590e9a917c39b86dddc38
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 12:04:43 +0530

    refactor(jsonplaceholder): generate json serialization for `User`

--- 003-jsonplaceholder-example/lib/features/users/models/user_model.dart
+++ 003-jsonplaceholder-example/lib/features/users/models/user_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// User model.
 class User {
   User({
@@ -15,4 +17,31 @@ class User {
   final String email;
   final String phone;
   final String website;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'username': username,
+      'email': email,
+      'phone': phone,
+      'website': website,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      username: map['username'] as String,
+      email: map['email'] as String,
+      phone: map['phone'] as String,
+      website: map['website'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
 }
