9c53bd6001fe81f319de5b2ae0f7cd96cedfc9e5
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 11:31:15 +0530

    refactor(jsonplaceholder): create `UserRepository`

---
+++ 003-jsonplaceholder-example/lib/features/users/reositories/user_repository.dart
@@ -0,0 +1,11 @@
+import 'package:jsonplaceholder_example/features/users/models/user_model.dart';
+
+class UserRepository {
+  Future<List<User>> getAllUsers() async {
+    throw UnimplementedError();
+  }
+
+  Future<User> getUserById(int id) async {
+    throw UnimplementedError();
+  }
+}
