eaec5ec57c498fcc9ce1520893e8ae4ac30556ab
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 19:42:13 +0530

    refactor(jsonplaceholder): create `AlbumRepository`

---
+++ 003-jsonplaceholder-example/lib/features/albums/repositories/albums_repository.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+
+import '../models/album_model.dart';
+
+class AlbumRepository {
+  /// Base url.
+  static const _base = 'https://jsonplaceholder.typicode.com';
+
+  Future<List<Album>> getAllAlbums() async {
+    try {
+      final res = await http.get(Uri.parse('$_base/albums'));
+      final rawAlbums = jsonDecode(res.body) as List;
+      return rawAlbums.map((e) => Album.fromMap(e)).toList();
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<Album> getAlbumById(int id) async {
+    try {
+      final res = await http.get(Uri.parse('$_base/albums/$id'));
+      final rawAlbum = json.decode(res.body);
+      return Album.fromMap(rawAlbum);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<List<Album>> getAllAlbumByUser(int userId) async {
+    try {
+      final res = await http.get(Uri.parse('$_base/users/$userId/albums'));
+      final rawAlbum = jsonDecode(res.body) as List;
+      return rawAlbum.map((e) => Album.fromMap(e)).toList();
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+}
