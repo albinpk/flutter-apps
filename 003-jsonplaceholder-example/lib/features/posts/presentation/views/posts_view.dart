4a0cb94db2e871a283d9ecc2bc589e171eb50ff5
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 15:14:15 +0530

    feat(jsonplaceholder): show posts in `PostsView`

--- 003-jsonplaceholder-example/lib/features/posts/presentation/views/posts_view.dart
+++ 003-jsonplaceholder-example/lib/features/posts/presentation/views/posts_view.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
-class PostsView extends StatelessWidget {
+import '../../models/post_model.dart';
+import '../../repositories/post_repository.dart';
+
+class PostsView extends StatefulWidget {
   const PostsView({super.key});
 
   @override
+  State<PostsView> createState() => _PostsViewState();
+}
+
+class _PostsViewState extends State<PostsView> {
+  final Future<List<Post>> _postsFuture = PostRepository().getAllPosts();
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Posts'),
+    return FutureBuilder<List<Post>>(
+      future: _postsFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) return const Center(child: Text('An error!'));
+
+        if (!snapshot.hasData) {
+          return const Center(child: Text('No data found!'));
+        }
+
+        final posts = snapshot.data!;
+
+        if (posts.isEmpty) {
+          return const Center(child: Text('No Posts found!'));
+        }
+
+        return ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            return Text(posts[index].title);
+          },
+        );
+      },
     );
   }
 }
