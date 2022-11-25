7a02c3247d3f4f8536d4cc76f5d8fcd545a2038e
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-25 13:03:02 +0530

    refactor(jsonplaceholder): create `Address` model

--- 003-jsonplaceholder-example/lib/features/users/models/user_model.dart
+++ 003-jsonplaceholder-example/lib/features/users/models/user_model.dart
@@ -2,11 +2,12 @@ import 'dart:convert';
 
 /// User model.
 class User {
-  User({
+  const User({
     required this.id,
     required this.name,
     required this.username,
     required this.email,
+    required this.address,
     required this.phone,
     required this.website,
   });
@@ -15,6 +16,7 @@ class User {
   final String name;
   final String username;
   final String email;
+  final Address address;
   final String phone;
   final String website;
 
@@ -24,6 +26,7 @@ class User {
       'name': name,
       'username': username,
       'email': email,
+      'address': address.toMap(),
       'phone': phone,
       'website': website,
     };
@@ -35,6 +38,7 @@ class User {
       name: map['name'] as String,
       username: map['username'] as String,
       email: map['email'] as String,
+      address: Address.fromMap(map['address'] as Map<String, dynamic>),
       phone: map['phone'] as String,
       website: map['website'] as String,
     );
@@ -44,4 +48,42 @@ class User {
 
   factory User.fromJson(String source) =>
       User.fromMap(json.decode(source) as Map<String, dynamic>);
+}
+
+/// User address model.
+class Address {
+  const Address({
+    required this.street,
+    required this.suite,
+    required this.city,
+    required this.zipcode,
+  });
+
+  final String street;
+  final String suite;
+  final String city;
+  final String zipcode;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'street': street,
+      'suite': suite,
+      'city': city,
+      'zipcode': zipcode,
+    };
+  }
+
+  factory Address.fromMap(Map<String, dynamic> map) {
+    return Address(
+      street: map['street'] as String,
+      suite: map['suite'] as String,
+      city: map['city'] as String,
+      zipcode: map['zipcode'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Address.fromJson(String source) =>
+      Address.fromMap(json.decode(source) as Map<String, dynamic>);
 }
