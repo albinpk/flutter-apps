a07a5462fd038e5c19f87cb7a9af9ea73a7a2d28
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-26 09:12:05 +0530

    refactor(jsonplaceholder): create `getAllCommentsOfPost` method in `PostRepository`

--- 003-jsonplaceholder-example/lib/features/posts/repositories/post_repository.dart
+++ 003-jsonplaceholder-example/lib/features/posts/repositories/post_repository.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 
+import '../models/comment_model.dart';
 import '../models/post_model.dart';
 
 class PostRepository {
@@ -33,12 +34,24 @@ class PostRepository {
     }
   }
 
-  /// Fetch all post by userId
+  /// Fetch all post by userId.
   Future<List<Post>> getAllPostsByUser(int userId) async {
     try {
       final res = await http.get(Uri.parse('$_base/users/$userId/posts'));
       final rawPosts = jsonDecode(res.body) as List;
       return rawPosts.map((e) => Post.fromMap(e)).toList();
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  /// Fetch all comments of a post.
+  Future<List<Comment>> getAllCommentsOfPost(int postId) async {
+    try {
+      final res = await http.get(Uri.parse('$_base/posts/$postId/comments'));
+      final rawComments = jsonDecode(res.body) as List;
+      return rawComments.map((e) => Comment.fromMap(e)).toList();
     } catch (e) {
       log(e.toString());
       rethrow;
