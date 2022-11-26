019cd08b0739c47efef9d8535fc192b764ce45dc
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-26 10:46:25 +0530

    feat(jsonplaceholder): use `NavigationRail` on large screen

--- 003-jsonplaceholder-example/lib/my_app.dart
+++ 003-jsonplaceholder-example/lib/my_app.dart
@@ -30,41 +30,68 @@ class _HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<_HomeScreen> {
-  static const List<Widget> _views = [
-    UsersView(),
-    PostsView(),
-    AlbumsView(),
-  ];
-  static const _titles = ['Users', 'Posts', 'Albums'];
+  static const Set<_Nav> _navItems = {
+    _Nav(label: 'Users', iconData: Icons.groups, view: UsersView()),
+    _Nav(label: 'Posts', iconData: Icons.newspaper_rounded, view: PostsView()),
+    _Nav(label: 'Albums', iconData: Icons.collections, view: AlbumsView()),
+  };
 
   /// Selected index.
   int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    final isLargeScreen = MediaQuery.of(context).size.width > 700;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(_titles[_currentIndex]),
+        title: Text(_navItems.elementAt(_currentIndex).label),
       ),
-      body: _views[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (i) => setState(() => _currentIndex = i),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.supervised_user_circle_sharp),
-            label: 'Users',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.newspaper_rounded),
-            label: 'Posts',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.collections),
-            label: 'Albums',
-          ),
+      body: Row(
+        children: [
+          if (isLargeScreen)
+            NavigationRail(
+              selectedIndex: _currentIndex,
+              onDestinationSelected: _onNavIndexChange,
+              labelType: NavigationRailLabelType.all,
+              destinations: _navItems
+                  .map(
+                    (e) => NavigationRailDestination(
+                      label: Text(e.label),
+                      icon: Icon(e.iconData),
+                    ),
+                  )
+                  .toList(),
+            ),
+          Expanded(child: _navItems.elementAt(_currentIndex).view),
         ],
       ),
+      bottomNavigationBar: isLargeScreen
+          ? null
+          : BottomNavigationBar(
+              currentIndex: _currentIndex,
+              onTap: _onNavIndexChange,
+              items: _navItems
+                  .map((e) => BottomNavigationBarItem(
+                        label: e.label,
+                        icon: Icon(e.iconData),
+                      ))
+                  .toList(),
+            ),
     );
   }
+
+  void _onNavIndexChange(int i) => setState(() => _currentIndex = i);
+}
+
+class _Nav {
+  const _Nav({
+    required this.label,
+    required this.iconData,
+    required this.view,
+  });
+
+  final String label;
+  final IconData iconData;
+  final Widget view;
 }
